08061a19fa47369c2e1e383a6ce76ec9e5d0ee48
Sk Mohammed Aadil <154332246+SkMohammedAadil@users.noreply.github.com>
2025-04-22 16:41:55 +0000

    dataTypes and description

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";  //treat all code JS code as newer version
+
+// alert (3+4) //getting error while running because we are using nodeJs not browser
+
+console.log("hello aadil")
+console.log(3+4)    //code readability should be high
+
+let name = "Aadil"
+let age = 23
+let isLoggedIn = false
+let state;
+
+//-----------primitive dataTypes--------//
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => uniqueness 
+
+
+
+//-----Object------//
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);   //Object
+console.log(typeof "javaScript");
+console.log(typeof age);
