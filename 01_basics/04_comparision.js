55033e9283258f91d44348e692fb0f4f303921bc
Sk Mohammed Aadil <154332246+SkMohammedAadil@users.noreply.github.com>
2025-04-23 14:02:23 +0000

    always avoid these types of conversion 💀

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);    
+
+// 👆👇 always avoid these two types of conversion
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === (Strict check)
+
+console.log("2" === 2);
