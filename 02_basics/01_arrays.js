08441a62ec0b466a84dee3c1b1fa9a13d9a9d4f7
Sk Mohammed Aadil <154332246+SkMohammedAadil@users.noreply.github.com>
2025-04-25 14:47:49 +0000

    Arrays 🧛

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,30 @@
+// Array
+
+const myArr = [9, 1, 6, 3, 8, 5]
+const myHeros =["Hulk", "Ben10", "Thor", "Groot"]
+
+const myArr2 = new Array(10, 20, 50, 85)
+//console.log(myArr[2]);
+
+// Array Methods
+
+//myArr.push(28)  // Adds elements to the Arraylist
+//myArr.push(34)
+//console.log(myArr);
+
+//myArr.pop()     // remove the last element from the list
+
+//myArr.unshift(0)    // use to push element in the list
+//console.log(myArr);
+
+// console.log(myArr.includes(10));    // use to check whether the element is present or not in the array list
+// console.log(myArr.indexOf(6) );     // use to check the index position the present element if not prese
+
+//myArr.shift()    // use to remove the push element from the list
+
+const newArr = myArr.join()
+
+console.log(typeof newArr);
+
+console.log(myArr);    
+console.log(newArr);
