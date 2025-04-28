3dc481ab89aea722b8bb57e206264bdeb1798901
Sk Mohammed Aadil <154332246+SkMohammedAadil@users.noreply.github.com>
2025-04-28 14:48:39 +0000

    json and destructuring 🎭

--- 02_basics/04_Object.js
+++ 02_basics/04_Object.js
@@ -32,6 +32,60 @@ const obj4 = {5: "e", 6: "f"}
 //const obj3 = Object.assign({}, obj1, obj2, obj4)
 
 const obj3 = {...obj1, ...obj2, ...obj4}        //SPREAD method
-console.log(obj3);
+//console.log(obj3);
 
 //console.log(obj3==obj1==obj2==obj4);
+
+
+const users = [
+    {
+        id: 1,
+        email: "a@gmail.com"
+    },
+    {
+        id: 2,
+        email: "b@gmail.com"
+    },
+    {
+        id: 3,
+        email: "c@gmail.com"
+    }
+]
+
+users[1].email
+//console.log(gPayUser);
+
+// console.log(Object.keys(gPayUser));
+// console.log(Object.values(gPayUser));
+// console.log(Object.entries(gPayUser));
+
+// console.log(gPayUser.hasOwnProperty('isLoggedIn'));
+// console.log(gPayUser.hasOwnProperty('id'));     // to check whether the property is present or not
+
+//++++++++++++  De-Structuring  ++++++++++++++++++++
+
+const youtube = {
+    coursename: "javascript",
+    price: "999",
+    tutor: "Sam"
+}
+
+// youtube.tutor
+const {tutor} = youtube
+
+//console.log(tutor);
+
+const {tutor: teacher} = youtube    // this process of changing the name of variable is called DESTRUCTING 
+console.log(teacher);
+
+// {
+//     name: "aadil",
+//     email: "xyz@gmail.com",
+//     id: "234245"
+// }
+
+[
+    {},
+    {},
+    {}
+]
