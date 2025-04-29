47a800bf7b3a82ad940afa79f6ef74d5d0dcbc0b
Sk Mohammed Aadil <skmohammedaadil762@gmail.com>
2025-04-29 11:02:21 +0530

    functions and their execution 😎

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,21 @@
+// console.log("A");
+// console.log("A");
+// console.log("D");
+// console.log("I");
+// console.log("L");
+
+
+function myName () {
+    console.log("A");
+    console.log("A");
+    console.log("D");
+    console.log("I");
+    console.log("L");
+}
+
+myName()
+/*
+    here only myName is referance of the function and
+    to execute we use parenthesis i.e., myName() and
+
+*/
