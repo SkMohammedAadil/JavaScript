2a8f008c7d68fd77f75a29f9c26ea7e95f543911
Sk Mohammed Aadil <skmohammedaadil762@gmail.com>
2025-04-29 11:51:54 +0530

    functions with if - else 🧭

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -13,9 +13,40 @@ function myName () {
     console.log("L");
 }
 
-myName()
+// myName()
 /*
     here only myName is referance of the function and
     to execute we use parenthesis i.e., myName() and
 
 */
+
+// function addNumbers(num1, num2) {
+//     console.log(num1 + num2);
+    
+// }
+
+//addNumbers()        //output is NaN
+
+function addNumbers(num1, num2) {
+    let result = num1 + num2
+    return result
+// console.log("aadil");   // after return statement nothing will be executed by console
+    
+    return num1 + num2
+}
+const result = addNumbers(10, 20)
+//console.log("the sum of two number is : ", result);
+
+//+++++++++++++++ If - Else +++++++++++++++++++++++++
+
+function loginMessage(username = "Sam"){
+    if (!username) {
+        console.log("please enter username");
+        return
+    }
+    return `${username} just logged in `
+}
+
+//console.log((loginMessage("Aadil")));     // if we leave the argument empty it will return (undefined)
+console.log((loginMessage()));
+
