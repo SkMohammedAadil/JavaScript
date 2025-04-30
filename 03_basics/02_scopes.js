726dd9ff39d7d146caed1f0393dc0fffc0c0f33f
Sk Mohammed Aadil <skmohammedaadil762@gmail.com>
2025-04-30 19:40:28 +0530

    Scopes and if clause

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,8 +4,50 @@ let a = 300     // GLOBAL scope
 if (true) {
     let a = 10
     const b =20
-    console.log("INNER: ",a);       //BLOCK scope
+    //console.log("INNER: ",a);       //BLOCK scope
 }
- console.log(a);
+ //console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "aadil"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+   // console.log(website);
+
+    two()
+    
+}
+//one()
+
+if (true) {
+    const username = "aadil"
+    if (username === "aadil") {
+        const website = "instagram"
+        //console.log(username + website);
+        
+    }
+   // console.log(website);     ERROR 1
+    
+}
+//console.log(username);        ERROR 2
+
+
+// +++++++++++++++++++ intresting +++++++++++++++++++++++++
+
+console.log(addone(5));
+function addone(value) {
+    return value + 1
+}
+
+const addTwo = function(value){     // the process of holding constant through variable is known as "HOISTING"
+    return value + 2                //hoisting occurs when we declare function before initialization.
+}
+
+console.log(addTwo(5));
