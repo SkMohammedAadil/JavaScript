5cc72e5483caaa767d57fb879d903078ef1e79d0
Sk Mohammed Aadil <154332246+SkMohammedAadil@users.noreply.github.com>
2025-05-09 03:57:55 +0000

    Arrrows and use of this function

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -32,3 +32,15 @@ const mic = () => {
     console.log(this);
     
 }
+
+//mic ()
+
+// const addTwo = (num1, num2) => {                    //Explicit RETURN
+//     return num1 + num2
+// }
+
+//const addTwo = (num1, num2) => num1 + num2          // implicit RETURN
+
+const addTwo = (num1, num2) => num1 + num2 
+
+console.log((addTwo(10, 20)));
