574aa0fce1bc647c38c36cf5b38ab06be285645f
Sk Mohammed Aadil <154332246+SkMohammedAadil@users.noreply.github.com>
2025-05-12 05:02:28 +0000

    Flow Control

---
+++ 04_control_flow/one.js
@@ -0,0 +1,40 @@
+//  If statement
+
+const isUserloggedIn = true
+const tempreature = 32
+if (tempreature > 34) {
+    //console.log("It's Summer");      
+}
+else
+{
+   // console.log("Winter is comming!!");
+}
+
+const hp = 99
+if (hp > 100) {
+    const power = "fly"
+    //console.log(`User power: ${power}`);
+} else {
+    const power = "Low hp"                      //always use const or let inside if and don't use var 
+    //console.log(`User power: ${power}`);
+}
+
+const balance = 1000
+//if (balance > 500) console.log("test");     //Implicit IF
+
+if (balance < 200) {
+    //console.log("less then 200");
+} else if (balance <= 1000) {
+   // console.log("less then 1000");   
+} else if (balance > 500) {
+    //console.log("greater then 500");
+} else {
+    //console.log("out of money");
+}
+
+const userLogedIn = true
+const debitCard = true
+
+if (condition) {
+    
+}
