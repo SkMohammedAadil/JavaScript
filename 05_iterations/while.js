ad6a6676690b9aba306312ff978b728537a83070
Sk Mohammed Aadil <154332246+SkMohammedAadil@users.noreply.github.com>
2025-07-29 15:44:35 +0000

    loops and all 🤪

---
+++ 05_iterations/while.js
@@ -0,0 +1,25 @@
+
+let a = 0;
+while (a <= 10) {
+    console.log(`Value of a is ${a}`);
+    a = a + 2
+}
+
+console.log("===============================================");
+
+
+let Array = ['Ben10' , 'Oggy' , 'Doremon']
+
+let b = 0 
+while (b < Array.length) {
+    console.log(`value of Array is ${Array[b]}`);
+    b++;
+}
+
+console.log("===================================================");
+
+let score = 267
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 270);
